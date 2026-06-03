016710c9c2481f13f0b55fdaa3e2af69b3d3da68
gustavomisk-dev <gustavo.misk@zilicred.com.br>
2026-06-03 11:16:03 -0300

    fix: atualizar prévia de formato com coluna Celular

--- plataforma/app.py
+++ plataforma/app.py
@@ -605,9 +605,9 @@ def _render_download_seguro(username: str, page: str, selected: str,
 
 
 _DUMMY_CLIENTES = pd.DataFrame([
-    {"Data e Hora do Processamento": "DD/MM/AAAA HH:MM", "CPF": "111.111.111-11", "Nome do Cliente": "Nome Sobrenome"},
-    {"Data e Hora do Processamento": "DD/MM/AAAA HH:MM", "CPF": "222.222.222-22", "Nome do Cliente": "Nome Sobrenome"},
-    {"Data e Hora do Processamento": "DD/MM/AAAA HH:MM", "CPF": "333.333.333-33", "Nome do Cliente": "Nome Sobrenome"},
+    {"Data e Hora do Processamento": "DD/MM/AAAA HH:MM", "CPF": "111.111.111-11", "Nome do Cliente": "Nome Sobrenome", "Celular": "5511999999999"},
+    {"Data e Hora do Processamento": "DD/MM/AAAA HH:MM", "CPF": "222.222.222-22", "Nome do Cliente": "Nome Sobrenome", "Celular": "5511999999999"},
+    {"Data e Hora do Processamento": "DD/MM/AAAA HH:MM", "CPF": "333.333.333-33", "Nome do Cliente": "Nome Sobrenome", "Celular": "5511999999999"},
 ])
 
 _DUMMY_CONVERSAO = pd.DataFrame([
